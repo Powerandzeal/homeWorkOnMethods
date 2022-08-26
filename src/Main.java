59e2b222ba1e1f06eba71b5288de3e0ed26b850a
Sergey P <seregapanin23703@gmail.com>
2022-08-26 16:03:09 +0300

    Ex 2 try

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        getYear(2024);
+        printYear(2024);
+        int currentYear = LocalDate.now().getYear();
+        checkOSAndCheckYearMade(currentYear, 0);
+
     }
-
-    public static void getYear(int year) {
-
+// Exercise 1
+    private static void printYear(int year) {
 
         if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
             System.out.println(year + " является высокосным годом");
@@ -13,14 +17,47 @@ public class Main {
             System.out.println(year + " не является высокосным годом");
         }
         //return year;
+    }
+    // Exercise 2
+    private static void checkOSAndCheckYearMade(int YearOfMade, int OSVersion) {
+
+
+        if (YearOfMade < 2015 && OSVersion == 0) {
+            System.out.println("Установите облегченную версию приложения для IOS по ссылке");
+        }
+        if (YearOfMade > 2015 && OSVersion == 0) {
+            System.out.println("Установите  версию приложения для IOS по ссылке");
+        }
+        if (YearOfMade < 2015 && OSVersion == 1) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+        if (YearOfMade > 2015 && OSVersion == 1) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
 
 
     }
 
+   /* public static void osAndYear(int typeOsVerison, int yearOfMade) {
+
+    }
+
+    public static int calculationDays(int deliveryDistant) {
+
+        int deliviryDays = 1;
 
 
-    //public static void checkyear(int year) {}
+        if (deliveryDistant > 20) {
+            deliviryDays++;
+        }
+        if (deliveryDistant > 60) {
+            deliviryDays++;
+        }
+        return deliviryDays;
+        //System.out.println("Потребуется " +deliviryDays+   " дня");
 
+        //System.out.println("Потребуется дней ");
+    }*/
 
 
     /***
